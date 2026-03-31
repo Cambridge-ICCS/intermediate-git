884e4e9a2ed27872b78b9337e8a37dbf6ee0f6d9
Jack Atkinson <jwa34@cam.ac.uk>
2026-03-31 16:08:47 +0100

    Add simple pyproject.toml and __init__.py files in preparation for the library.
    Co-authored-by: joewallwork <jw2423@cam.ac.uk>

---
+++ thermolib/__init__.py
@@ -0,0 +1,9 @@
+"""
+Climate Thermodynamics Library.
+
+A collection of basic thermodynamic relations for climate science applications.
+
+Note: This library is designed for educational purposes in teaching git
+version control concepts using realistic climate science code.
+It is not intended for real research use.
+"""
